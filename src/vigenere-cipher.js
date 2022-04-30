0ee9fb75cbd45f46ece7e0d3ee3946f9fec09235
Vitaly Undro <vitalyundro@gmail.com>
2022-04-30 11:30:55 +0300

    fit: start task decoder

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,104 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+const englishLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const START_POSITION_CHARCODE = 65;
+function createArr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let deleteEl = arr[i].splice(0, i);
+    arr[i].push(...deleteEl);
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return arr;
+}
+const array = createArr(Array.from({ length: englishLetters.length }, () => [...englishLetters]));
+
+class VigenereCipheringMachine {
+  constructor(isDirect = true) {
+    this._reverseFlag = !isDirect;
+  }
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) throw Error("Incorrect arguments!");
+    const keyUC = key.toUpperCase();
+    const keyUCDried = keyUC.replace(/\s/g, "");
+
+    const messageUC = message.toUpperCase();
+    const messageUCDried = messageUC.trim();
+
+    const res = [];
+
+    const keyLength = keyUCDried.length;
+    const msgLength = messageUCDried.length;
+
+    for (let i = 0, n = 0; i < msgLength; i += 1, n += 1) {
+      const messageChar = messageUCDried[i];
+      if (!~englishLetters.indexOf(messageChar)) {
+        res.push(messageChar);
+        n--;
+        continue;
+      }
+      const messageCharCode = messageChar.charCodeAt(0);
+      const shiftedRowIndex = keyUCDried.charCodeAt(n % keyLength) - START_POSITION_CHARCODE;
+      const shiftedCharIndex = messageCharCode - START_POSITION_CHARCODE;
+      res.push(array[shiftedRowIndex][shiftedCharIndex]);
+    }
+    if (this._reverseFlag === true) return res.reverse().join("");
+    return res.join();
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage == undefined || key == undefined) throw Error("Incorrect arguments!");
+
+    const keyUC = key.toUpperCase();
+    const keyUCDried = keyUC.replace(/\s/g, "");
+
+    const encryptedMessageUC = encryptedMessage.toUpperCase();
+    const encryptedMessageUCDried = encryptedMessageUC.trim();
+
+    const keyLength = keyUCDried.length;
+    const msgLength = encryptedMessageUCDried.length;
+
+    const res = [];
+
+    for (let i = 0, n = 0; i < msgLength; i++, n++) {
+      let encryptedMessageChar = encryptedMessageUCDried[i];
+
+      const rowIndex = keyUCDried.charCodeAt(n % keyLength) - START_POSITION_CHARCODE;
+
+      const row = array[rowIndex];
+
+      if (!~englishLetters.indexOf(encryptedMessageChar)) {
+        res.push(encryptedMessageChar);
+        n--;
+        continue;
+      }
+      const targetIndex = row.indexOf(encryptedMessageChar);
+      const unshiftedRow = array[0];
+      const trueChar = unshiftedRow[targetIndex];
+
+      res.push(trueChar);
+    }
+    if (this._reverseFlag === true) return res.reverse().join("");
+    return res.join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
